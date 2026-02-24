c0ce87cfcc3e5524c917098e89b4a031fd7f42ce
ritu jha <ritu05491@gmail.com>
2026-02-24 12:41:58 +0530

    refactor: reorganize component imports and structure in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,22 @@
+import About from './components/About'
+import Contact from './components/Contact'
+import Footor from './components/Footor'
 import Header from './components/Header'
+import Hero from './components/Hero'
+import Projects from './components/Projects'
+import Skills from './components/Skills'
 
 function App() {
   
   return (
     <>
       <Header />
+      < Hero />
+      < About />
+      <Skills/>
+      < Projects />
+      {/* <Contact /> */}
+      <Footor/>
     </>
   )
 }
